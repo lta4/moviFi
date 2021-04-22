a17608a7d4367f6699f04fb1874ad61959e583d2
Andrew <andrew.mk4@yahoo.com>
2021-04-22 01:18:52 -0700

    Authentification Start

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import './App.css';
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Main from "./pages/main"
 import Nav from "./components/nav";
 import Footer from "./pages/footer";
 import Fav from "./pages/fav";
+import Dashboard from "./components/dashboard";
+import Preferences from "./components/preferences";
 
 function App() {
 
   return (
       <div className="App">
         <Nav />
+        <BrowserRouter>
         <Switch>
+          <Route path="/dashboard">
+            <Dashboard className="dashboardRoute"/>
+          </Route>
+          <Route path="/preferences">
+            <Preferences className="preferencesRoute"/>
+          </Route>
           <Route path="/fav">
             <Fav className="favRoute" />
           </Route>
@@ -19,6 +28,7 @@ function App() {
             <Main className="mainRoute" />
           </Route>
         </Switch>
+        </BrowserRouter>
         <Footer />
       </div>
   );
