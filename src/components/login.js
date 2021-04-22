72b1152e3f32d17a450e9452000773b84846ba94
Andrew <andrew.mk4@yahoo.com>
2021-04-22 14:44:49 -0700

    login ready

--- src/components/login.js
+++ src/components/login.js
@@ -1,18 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Login.css";
+import PropTypes from "prop-types";
 
-const Login = () => {
+async function loginUser(credentials) {
+    return fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(data => data.json())
+}
+export default function Login({setToken}) {
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
     return (
         <div className="login-wrapper">
             <h1>Please Log In</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" />
+                    <input type="text" onChange={e => setUserName(e.target.value)}/>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" />
+                    <input type="password" onChange={e => setPassword(e.target.value)}/>
                 </label>
                 <div>
                     <button type="submit">Submit</button>
@@ -22,4 +45,6 @@ const Login = () => {
     );
 };
 
-export default Login
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
+}
