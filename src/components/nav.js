bdfb7395799d967aaffedeb647d39375bf694169
Andrew <andrew.mk4@yahoo.com>
2021-04-21 21:39:14 -0700

    nav links

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 
 const Nav = (props) => {
     return (
-        <div>
-            <h1 className="navBar">MoviFi</h1>
+        <div className="navBar">
             <Link to="/">
+                <h1 className="title">MoviFi</h1>
+            </Link>
+            <Link to="/fav">
                 <div className="fav">Fav</div>
             </Link>
         </div>
