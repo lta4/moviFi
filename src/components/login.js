e50432e9bc532094365a047a1324c94f201090c7
Andrew <andrew.mk4@yahoo.com>
2021-05-05 03:16:02 -0700

    update to login

--- src/components/login.js
+++ src/components/login.js
@@ -27,18 +27,18 @@ export default function Login({setToken}) {
 
     return (
         <div className="login-wrapper">
-            <h1>Please Log In</h1>
+            <h1>Login</h1>
             <form onSubmit={handleSubmit}>
                 <label>
                     <p>Username</p>
-                    <input type="text" onChange={e => setUserName(e.target.value)}/>
+                    <input type="text" placeholder="Type your username" onChange={e => setUserName(e.target.value)}/>
                 </label>
                 <label>
                     <p>Password</p>
-                    <input type="password" onChange={e => setPassword(e.target.value)}/>
+                    <input type="password" placeholder="Type your password" onChange={e => setPassword(e.target.value)}/>
                 </label>
                 <div className="submit-wrapper">
-                    <button type="submit" className="submit">Submit</button>
+                    <button type="submit" className="submit">Login</button>
                 </div>
             </form>
         </div>
