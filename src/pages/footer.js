ae8d07fd1043d2bb44d83fdd177fc2ce2755c0ee
Andrew <andrew.mk4@yahoo.com>
2021-08-28 03:06:15 -0700

    foot update

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -2,9 +2,17 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div>
-            <h2 className="footerBar"><a href=".navBar" id="footLink">MoviFi</a></h2>
-        </div>
+        // <div>
+        //     <h2 className="footerBar"><a href=".navBar" id="footLink">MoviFi</a></h2>
+        //     <p className="copyRight">Andrew Arias 2021</p>
+        // </div>
+        <footer 
+        className="footer"
+      >
+        © {new Date().getFullYear()}, Built with love by
+        {` `}
+        <a href="mailto:andrew4ld@gmail.com" className="footLink">Andrew Arias</a>
+      </footer>
     );
 };
 
