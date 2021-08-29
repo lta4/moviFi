274d15f32d6977e416e545502dd66a3b70b928c9
Andrew <andrew.mk4@yahoo.com>
2021-08-29 03:51:44 -0700

    update index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SocialIcon } from 'react-social-icons';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
 
-ReactDOM.render(
+ReactDOM.render(<SocialIcon url="https://twitter.com/jaketrent" />,
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById.body('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
