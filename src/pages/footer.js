bbb79b21f945051900776f0965dc83e48d7514be
Andrew <andrew.mk4@yahoo.com>
2021-09-01 01:03:45 -0700

    update

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -6,12 +6,10 @@ const Footer = () => {
         //     <h2 className="footerBar"><a href=".navBar" id="footLink">MoviFi</a></h2>
         //     <p className="copyRight">Andrew Arias 2021</p>
         // </div>
-        <footer 
-        className="footer"
-      >
-        © {new Date().getFullYear()}, Built with love by
-        {` `}
-        <a href="mailto:andrew4ld@gmail.com" className="footLink">Andrew Arias</a>
+        <footer className="footer">
+          © {new Date().getFullYear()}, Built with love by 
+          {` `}
+          <a href="mailto:andrew4ld@gmail.com" className="footLink">Andrew Arias</a>
       </footer>
     );
 };
